ed937cd11d6c4417a617113d67906ac75da27e72
Emilton Navas <emiltonnavas@unicolombo.edu.co>
2025-03-07 16:02:35 -0500

    first commit

---
+++ ServidorTcp/src/EmiltonNavas/imc/Principal.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package EmiltonNavas.imc;
+import EmiltonNavas.imc.vistas.VentanaPrincipal;
+/**
+ *
+ * @author Emilton Navas
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        VentanaPrincipal v = new VentanaPrincipal();
+        v.setLocationRelativeTo(null);
+        v.setVisible(true);
+    }
+    
+}
